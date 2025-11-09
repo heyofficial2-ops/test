595a95bb99094db047a0c1a5dbfdb738af4f5545
heyofficial2-ops <heyofficial2@gmail.com>
2025-11-09 07:19:20 +0000

    Disable factorial function and its usage
    Comment out the factorial function and its call in main.

--- Tasks.java
+++ Tasks.java
@@ -11,17 +11,17 @@ public class Tasks {
     }
 
     // Function 3: Calculate the factorial of a number
-    public static int factorial(int n) {
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
+//    public static int factorial(int n) {
+//      int result = 1;
+  //      for (int i = 1; i <= n; i++) {
+    //        result *= i;
+      //  }
+        //return result;
+    //}
 
     public static void main(String[] args) {
         System.out.println("Reversed: " + reverse("Hello"));
         System.out.println("Max: " + max(10, 25));
-        System.out.println("Factorial: " + factorial(5));
+        //System.out.println("Factorial: " + factorial(5));
     }
 }
