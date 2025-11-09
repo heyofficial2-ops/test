70d676511c09b46a24ea98881aed3fe47411d78a
heyofficial2-ops <heyofficial2@gmail.com>
2025-11-09 07:34:48 +0000

    Uncomment factorial function and related code

--- Tasks.java
+++ Tasks.java
@@ -1,27 +1,27 @@
 public class Tasks {
 
     // Function 1: Reverse a string
-    public static String reverse(String text) {
-        return new StringBuilder(text).reverse().toString();
-    }
-
-    // Function 2: Find the maximum of two numbers
-    public static int max(int a, int b) {
-        return (a > b) ? a : b;
-    }
-
-    // Function 3: Calculate the factorial of a number
-//    public static int factorial(int n) {
-//      int result = 1;
-  //      for (int i = 1; i <= n; i++) {
-    //        result *= i;
-      //  }
-        //return result;
+    //public static String reverse(String text) {
+//    return new StringBuilder(text).reverse().toString();
     //}
 
+    // Function 2: Find the maximum of two numbers
+    //public static int max(int a, int b) {
+      //  return (a > b) ? a : b;
+    //}
+
+    // Function 3: Calculate the factorial of a number
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Reversed: " + reverse("Hello"));
-        System.out.println("Max: " + max(10, 25));
-        //System.out.println("Factorial: " + factorial(5));
+    //    System.out.println("Reversed: " + reverse("Hello"));
+      //  System.out.println("Max: " + max(10, 25));
+        System.out.println("Factorial: " + factorial(5));
     }
 }
