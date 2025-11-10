94c393730470d84923656110117d48f3d7a14c85
heyofficial2-ops <heyofficial2@gmail.com>
2025-11-10 06:07:37 +0000

    Update cal.py

--- cal.py
+++ cal.py
@@ -4,13 +4,13 @@ def add(a, b):
 def subtract(a, b):
     return a - b
     
-def multiply(a, b):
-    return a * b
+#def multiply(a, b):
+ #   return a * b
 
-def divide(a, b):
-    if b == 0:
-        return "Error: Division by zero"
-    return a / b
+#def divide(a, b):
+ #   if b == 0:
+  #      return "Error: Division by zero"
+   # return a / b
 
 def main():
     num1 = 10
@@ -18,12 +18,12 @@ def main():
     
     print(f"{num1} + {num2} = {add(num1, num2)}")
     print(f"{num1} - {num2} = {subtract(num1, num2)}")
-    print(f"{num1} * {num2} = {multiply(num1, num2)}")
-    print(f"{num1} / {num2} = {divide(num1, num2)}")
+    #print(f"{num1} * {num2} = {multiply(num1, num2)}")
+    #print(f"{num1} / {num2} = {divide(num1, num2)}")
 
     #Test divide by zero
-    num2 = 0
-    print(f"{num1} / {num2} = {divide(num1, num2)}")
+    #num2 = 0
+    #print(f"{num1} / {num2} = {divide(num1, num2)}")
 
 # Call main function to run the program
 if __name__ == "__main__":
