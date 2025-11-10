62c5e14e72a2a13740833b58e4910d3de439dd50
heyofficial2-ops <heyofficial2@gmail.com>
2025-11-10 05:52:41 +0000

    Uncomment string reversal and max functions

--- Tasks.java
+++ Tasks.java
@@ -1,14 +1,14 @@
 public class Tasks {
 
     // Function 1: Reverse a string
-    //public static String reverse(String text) {
-//    return new StringBuilder(text).reverse().toString();
-    //}
+    public static String reverse(String text) {
+        return new StringBuilder(text).reverse().toString();
+    }
 
     // Function 2: Find the maximum of two numbers
-    //public static int max(int a, int b) {
-      //  return (a > b) ? a : b;
-    //}
+    public static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
 
     // Function 3: Calculate the factorial of a number
     public static int factorial(int n) {
@@ -20,8 +20,8 @@ public class Tasks {
     }
 
     public static void main(String[] args) {
-    //    System.out.println("Reversed: " + reverse("Hello"));
-      //  System.out.println("Max: " + max(10, 25));
+        System.out.println("Reversed: " + reverse("Hello"));
+        System.out.println("Max: " + max(10, 25));
         System.out.println("Factorial: " + factorial(5));
     }
 }
