ba912a93571aeb4f66a9d6f6390bf301d12e9b81
heyofficial2-ops <heyofficial2@gmail.com>
2026-02-01 16:08:10 +0000

    Comment out add function and related print statement
    Comment out the add function and its usage in main.

--- cal.py
+++ cal.py
@@ -1,5 +1,5 @@
-def add(a, b):
-    return a + b
+#def add(a, b):
+ #   return a + b
 
 def subtract(a, b):
     return a - b
@@ -16,7 +16,7 @@ def main():
     num1 = 10
     num2 = 5
     
-    print(f"{num1} + {num2} = {add(num1, num2)}")
+    #print(f"{num1} + {num2} = {add(num1, num2)}")
     print(f"{num1} - {num2} = {subtract(num1, num2)}")
     #print(f"{num1} * {num2} = {multiply(num1, num2)}")
     #print(f"{num1} / {num2} = {divide(num1, num2)}")
